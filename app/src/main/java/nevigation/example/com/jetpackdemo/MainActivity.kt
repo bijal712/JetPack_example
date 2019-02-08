8acdd28e467fbeaa4de8141f91d9fcbe9a5ab5e6
bijumittu <bijalsutariya712.bs@gmail.com>
2019-02-08 17:33:07 +0530

    Initial commit

---
+++ app/src/main/java/nevigation/example/com/jetpackdemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package nevigation.example.com.jetpackdemo
+
+import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawer: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        /*if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }*/
+        val host = supportFragmentManager.findFragmentById(R.id.my_fragment) as NavHostFragment?
+                ?: return
+
+        val navController = host.navController
+        drawer = findViewById(R.id.container)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawer)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(drawer, Navigation.findNavController(this,R.id.my_fragment))
+    }
+}
---
+++ app/src/main/java/nevigation/example/com/jetpackdemo/ui/main/MainFragment.kt
@@ -0,0 +1,38 @@
+package nevigation.example.com.jetpackdemo.ui.main
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.main_fragment.*
+import nevigation.example.com.jetpackdemo.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        btnFirst.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_firstFragment2) //action id
+        }
+
+        btnSecond.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+    }
+
+}
